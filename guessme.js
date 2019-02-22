cb7bd0ce773194f10ce30f8882670ea4ab47be71
Karunagaran Ulaganathan <karunagu@amazon.com>
2019-02-21 20:12:50 -0800

    Added while loop with sixth question

--- guessme.js
+++ guessme.js
@@ -55,6 +55,32 @@ if (guess === 'Y' || guess === 'y' ) {
     else {
         alert('Hi '+ user +'! Sorry, none of your guesses are correct, try again' );
     } 
+
+    var fav_num;
+    var numtimes = 1;
+
+    while (fav_num !== 33) {
+        fav_num = parseInt(prompt('Can you guess my favorite number?'));
+    
+    if (fav_num < 20) {                                     /* Too Low */
+        alert('Sorry! You guessed too low!');
+        numtimes++;
+    } else if (fav_num >= 20 && fav_num < 33) {             /*  Low */
+        alert('Sorry! You guessed low!');
+        numtimes++;
+    } else if (fav_num > 33 && fav_num < 53) {              /* High */
+        alert('Sorry! You guessed high!');
+        numtimes++;
+    } else if (fav_num >= 53                 ) {             /*  Too High */
+        alert('Sorry! You guessed Too high!');
+        numtimes++;
+    } else if (isNaN(fav_num) || fav_num === null) {
+        alert('Please enter an actual number');
+        numtimes++;
+    }
+}
+    console.log('Total number of guesses until you got it:', numtimes);
+    alert('Hi '+ user +'It took '+ numtimes+ ' guesses until you got it ');
 }
 else {
     alert('Hi '+ user +'! No problem, try again later');
